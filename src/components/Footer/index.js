d14c3e3b930506c2ac759d9a4bc49f66a18b59fa
olivertoussaint <toussaint.olivier66@gmail.com>
2022-01-22 17:12:09 +0100

    create and add newcomponents

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+        <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par Olivier-Toussaint - 2022</p>
+                <p>Les icônes ont été récupérées sur iconFinder.com</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Landing/index.js
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect, Fragment } from 'react';
+
+const Landing = () => {
+
+    const [btn, setBtn] = useState(false);
+
+   const refWolverine = useRef(null);
+
+   useEffect(() => {
+        refWolverine.current.classList.add("startingImg");
+        setTimeout(() => {
+            refWolverine.current.classList.remove("startingImg");
+            setBtn(true);
+        }, 1000);
+
+   }, [])
+
+   const setLeftImg = () => {
+    refWolverine.current.classList.add("leftImg");
+   }
+
+   const setRightImg = () => {
+    refWolverine.current.classList.add("rightImg");
+   }
+
+   const clearImg = () => {
+        if(refWolverine.current.classList.contains("leftImg")) {
+            refWolverine.current.classList.remove("leftImg")
+        } else if (refWolverine.current.classList.contains("rightImg")) {
+            refWolverine.current.classList.remove("rightImg")
+        }
+   }
+   
+   const displayBtn = btn && (
+        <Fragment>
+            <div onMouseOver={setLeftImg} onMouseOut={clearImg} className='leftBox'>
+                <button className='btn-welcome'>Inscription</button>
+            </div>
+            <div onMouseOver={setRightImg} onMouseOut={clearImg} className='rightBox'>
+                <button className='btn-welcome'>Connexion</button>
+            </div>
+        </Fragment>
+   )
+
+  return (
+    <main ref={refWolverine} className='welcomePage'>
+        { displayBtn }
+    </main>
+  ) 
+}
+
+export default Landing;
